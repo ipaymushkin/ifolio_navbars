9a2866e5cd0551c1b9849b2932da5f87a9c9a47c
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-08-03 10:39:42 +0300

    change exports

---
+++ index.jsx
@@ -0,0 +1,5 @@
+import NavBar from "./src/modules/NavBar";
+import NavBarMobile from "./src/modules/NavBarMobile";
+
+export {NavBar};
+export {NavBarMobile};
--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import NavBar from "./modules/NavBar";
-import NavBarMobile from "./modules/NavBarMobile";
-
-export {NavBar};
-export {NavBarMobile};
