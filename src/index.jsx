9c8436cb333664a4719a8394bef7ab06ceef3b7f
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-08-03 10:26:23 +0300

    change exports

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import NavBar from "./modules/NavBar";
+import NavBarMobile from "./modules/NavBarMobile";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div/>
-);
+export {NavBar};
+export {NavBarMobile};
