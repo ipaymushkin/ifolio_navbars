0531920f75a5a0d8f2761708d692a2df90ee0091
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-08-04 15:19:40 +0300

    1.7.1

--- dist/modules/NavBarMobile/index.js
+++ dist/modules/NavBarMobile/index.js
@@ -2,6 +2,8 @@
 
 var _interopRequireDefault = require("C:/Projects/ifolio_navbars/node_modules/@babel/runtime/helpers/interopRequireDefault.js").default;
 
+var _interopRequireWildcard = require("C:/Projects/ifolio_navbars/node_modules/@babel/runtime/helpers/interopRequireWildcard.js").default;
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -11,7 +13,7 @@ var _taggedTemplateLiteral2 = _interopRequireDefault(require("C:/Projects/ifolio
 
 var _react = _interopRequireDefault(require("react"));
 
-var _styledComponents = _interopRequireDefault(require("styled-components"));
+var _styledComponents = _interopRequireWildcard(require("styled-components"));
 
 var _getCropperQueryString = require("../../utils/getCropperQueryString");
 
@@ -23,18 +25,23 @@ var _BurgerIcon = _interopRequireDefault(require("../../icons/BurgerIcon"));
 
 var _CropImage = _interopRequireDefault(require("../../components/CropImage"));
 
-var _templateObject, _templateObject2, _templateObject3, _templateObject4;
+var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5;
 
 var NavBarMobile = function NavBarMobile(_ref) {
-  var config = _ref.config;
+  var config = _ref.config,
+      _ref$stickyOffset = _ref.stickyOffset,
+      stickyOffset = _ref$stickyOffset === void 0 ? 0 : _ref$stickyOffset;
   var mobileHeight = config.mobileHeight,
       navBarRegularColor = config.navBarRegularColor,
       mobileLogoAlignment = config.mobileLogoAlignment,
       logoSrc = config.logoSrc,
-      logoCropperOptions = config.logoCropperOptions;
+      logoCropperOptions = config.logoCropperOptions,
+      navBarBehavior = config.navBarBehavior;
   return /*#__PURE__*/_react.default.createElement(Wrapper, {
     height: mobileHeight,
-    navBarRegularColor: navBarRegularColor
+    navBarRegularColor: navBarRegularColor,
+    navBarBehavior: navBarBehavior,
+    stickyOffset: stickyOffset
   }, /*#__PURE__*/_react.default.createElement(IconWrapper, null, /*#__PURE__*/_react.default.createElement(_Icon.Icon, {
     icon: _BurgerIcon.default,
     color: '#ffffff'
@@ -46,16 +53,20 @@ var NavBarMobile = function NavBarMobile(_ref) {
   }))));
 };
 
-var Wrapper = _styledComponents.default.div(_templateObject || (_templateObject = (0, _taggedTemplateLiteral2.default)(["\n  display: flex;\n  width: 240px;\n  padding: 15px 13px 16px 15px;\n  border-radius: 3px;\n  border: solid 1px rgba(164, 198, 225, 0.39);\n  height: ", "px;\n  background-color: ", ";\n  align-items: center;\n  position: relative;\n"])), function (_ref2) {
+var Wrapper = _styledComponents.default.div(_templateObject || (_templateObject = (0, _taggedTemplateLiteral2.default)(["\n  display: flex;\n  width: 100%;\n  padding: 15px 13px 16px 15px;\n  border-radius: 3px;\n  border: solid 1px rgba(164, 198, 225, 0.39);\n  height: ", "px;\n  background-color: ", ";\n  align-items: center;\n  position: relative;\n  ", ";\n"])), function (_ref2) {
   var height = _ref2.height;
   return height;
 }, function (_ref3) {
   var navBarRegularColor = _ref3.navBarRegularColor;
   return navBarRegularColor;
+}, function (_ref4) {
+  var navBarBehavior = _ref4.navBarBehavior,
+      stickyOffset = _ref4.stickyOffset;
+  return navBarBehavior === 'frozen' && (0, _styledComponents.css)(_templateObject2 || (_templateObject2 = (0, _taggedTemplateLiteral2.default)(["\n            position: sticky;\n            top: ", "px;\n          "])), stickyOffset);
 });
 
-var LogoWrapper = _styledComponents.default.div(_templateObject2 || (_templateObject2 = (0, _taggedTemplateLiteral2.default)(["\n  display: flex;\n  flex: 1 1 auto;\n  justify-content: ", ";\n"])), function (_ref4) {
-  var mobileLogoAlignment = _ref4.mobileLogoAlignment;
+var LogoWrapper = _styledComponents.default.div(_templateObject3 || (_templateObject3 = (0, _taggedTemplateLiteral2.default)(["\n  display: flex;\n  flex: 1 1 auto;\n  justify-content: ", ";\n"])), function (_ref5) {
+  var mobileLogoAlignment = _ref5.mobileLogoAlignment;
 
   if (mobileLogoAlignment === 'center') {
     return 'center';
@@ -66,9 +77,9 @@ var LogoWrapper = _styledComponents.default.div(_templateObject2 || (_templateOb
   return '';
 });
 
-var Logo = _styledComponents.default.div(_templateObject3 || (_templateObject3 = (0, _taggedTemplateLiteral2.default)(["\n  width: ", "px;\n  height: ", "px;\n"])), _consts.logoImageWidth, _consts.logoImageHeight);
+var Logo = _styledComponents.default.div(_templateObject4 || (_templateObject4 = (0, _taggedTemplateLiteral2.default)(["\n  width: ", "px;\n  height: ", "px;\n"])), _consts.logoImageWidth, _consts.logoImageHeight);
 
-var IconWrapper = _styledComponents.default.div(_templateObject4 || (_templateObject4 = (0, _taggedTemplateLiteral2.default)(["\n  display: flex;\n  position: absolute;\n"])));
+var IconWrapper = _styledComponents.default.div(_templateObject5 || (_templateObject5 = (0, _taggedTemplateLiteral2.default)(["\n  display: flex;\n  position: absolute;\n"])));
 
 var _default = NavBarMobile;
 exports.default = _default;
--- src/modules/NavBarMobile/index.jsx
+++ src/modules/NavBarMobile/index.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { getCropperQueryString } from '../../utils/getCropperQueryString';
-import { logoImageHeight, logoImageWidth } from '../../consts';
-import { Icon } from '../../components/Icon';
+import styled, {css} from 'styled-components';
+import {getCropperQueryString} from '../../utils/getCropperQueryString';
+import {logoImageHeight, logoImageWidth} from '../../consts';
+import {Icon} from '../../components/Icon';
 import BurgerIcon from '../../icons/BurgerIcon';
 import CropImage from '../../components/CropImage';
 
-const NavBarMobile = ({ config }) => {
-  const { mobileHeight, navBarRegularColor, mobileLogoAlignment, logoSrc, logoCropperOptions } = config;
-  return (
-    <Wrapper height={mobileHeight} navBarRegularColor={navBarRegularColor}>
-      <IconWrapper>
-        <Icon icon={BurgerIcon} color={'#ffffff'} />
-      </IconWrapper>
-      <LogoWrapper mobileLogoAlignment={mobileLogoAlignment}>
-        <Logo>
-          <CropImage source={logoSrc + getCropperQueryString(logoCropperOptions)} crop={logoCropperOptions} />
-        </Logo>
-      </LogoWrapper>
-    </Wrapper>
-  );
+const NavBarMobile = ({config, stickyOffset = 0}) => {
+    const {mobileHeight, navBarRegularColor, mobileLogoAlignment, logoSrc, logoCropperOptions, navBarBehavior} = config;
+    return (
+        <Wrapper height={mobileHeight} navBarRegularColor={navBarRegularColor} navBarBehavior={navBarBehavior} stickyOffset={stickyOffset}>
+            <IconWrapper>
+                <Icon icon={BurgerIcon} color={'#ffffff'}/>
+            </IconWrapper>
+            <LogoWrapper mobileLogoAlignment={mobileLogoAlignment}>
+                <Logo>
+                    <CropImage source={logoSrc + getCropperQueryString(logoCropperOptions)} crop={logoCropperOptions}/>
+                </Logo>
+            </LogoWrapper>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
@@ -29,16 +29,22 @@ const Wrapper = styled.div`
   padding: 15px 13px 16px 15px;
   border-radius: 3px;
   border: solid 1px rgba(164, 198, 225, 0.39);
-  height: ${({ height }) => height}px;
-  background-color: ${({ navBarRegularColor }) => navBarRegularColor};
+  height: ${({height}) => height}px;
+  background-color: ${({navBarRegularColor}) => navBarRegularColor};
   align-items: center;
   position: relative;
+  ${({navBarBehavior, stickyOffset}) =>
+          navBarBehavior === 'frozen' &&
+          css`
+            position: sticky;
+            top: ${stickyOffset}px;
+          `};
 `;
 
 const LogoWrapper = styled.div`
   display: flex;
   flex: 1 1 auto;
-  justify-content: ${({ mobileLogoAlignment }) => {
+  justify-content: ${({mobileLogoAlignment}) => {
     if (mobileLogoAlignment === 'center') {
       return 'center';
     } else if (mobileLogoAlignment === 'right') {
@@ -59,7 +65,8 @@ const IconWrapper = styled.div`
 `;
 
 NavBarMobile.propTypes = {
-  config: PropTypes.object
+    config: PropTypes.object,
+    stickyOffset: PropTypes.number,
 };
 
 export default NavBarMobile;
