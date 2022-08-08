589ef1e99adac1e0a7ef557dc573c507e6c6d9f2
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-08-08 19:13:35 +0300

    change config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 import { babel } from '@rollup/plugin-babel';
@@ -38,10 +37,5 @@ export default [
             resolve(),
             terser(),
         ],
-    },
-    {
-        input: "dist/index.esm.js",
-        output: [{ file: "dist/index.js", format: "esm" }],
-        plugins: [dts()],
     },
 ];
