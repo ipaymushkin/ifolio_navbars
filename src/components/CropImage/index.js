bf3abd3b4f1670bd617b38d5a8f694f392d50412
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-10-24 11:44:07 +0300

    add new styles

--- src/components/CropImage/index.js
+++ src/components/CropImage/index.js
@@ -2,14 +2,14 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
-const Index = forwardRef(({ height, id, crop, source, alt, ignoreMaxHeight, isTurntable, isHero }, ref) => {
+const Index = forwardRef(({ height, id, crop, source, alt, ignoreMaxHeight }, ref) => {
   const styles = { width: '100%', height: '100%', objectFit: 'contain' };
   if (!ignoreMaxHeight) {
     styles.maxHeight = '600px';
   }
   return (
-    <CropArea ref={ref} isTurntable={isTurntable} isHero={isHero} height={height} id={id}>
-      <CropImageWrapper {...crop.imagePosition} isHero={isHero}>
+    <CropArea ref={ref} height={height} id={id}>
+      <CropImageWrapper {...crop.imagePosition}>
         <img src={source} alt={alt} style={styles} />
       </CropImageWrapper>
     </CropArea>
@@ -22,24 +22,13 @@ const CropImageWrapper = styled.div`
   top: ${({ y }) => y || 0}%;
   width: ${({ width }) => width || 100}%;
   height: ${({ height }) => height || 100}%;
-
-  ${({ isHero }) =>
-    isHero &&
-    css`
-      position: static;
-      img {
-        object-fit: cover;
-        //margin-left: auto;
-        //margin-right: auto;
-        //width: auto !important;
-      }
-    `};
+  text-align: center;
 `;
 
 const CropArea = styled.div`
   width: 100%;
-  height: ${({ height, isTurntable, isHero }) =>
-    isTurntable ? '100%' : isHero ? '100%' : height ? height + 'px' : '100%'};
+  height: ${({ height }) =>
+    height ? height + 'px' : '100%'};
   position: relative;
   background-color: transparent;
   overflow: hidden;
@@ -52,8 +41,6 @@ Index.propTypes = {
   source: PropTypes.string.isRequired,
   alt: PropTypes.string,
   ignoreMaxHeight: PropTypes.bool,
-  isTurntable: PropTypes.bool,
-  isHero: PropTypes.bool
 };
 
 Index.defaultProps = {
