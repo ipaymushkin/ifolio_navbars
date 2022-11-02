ac469e5abe74fcc9ad28c209ac03343c55c4e39c
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-11-02 10:22:15 +0300

    change config

--- src/consts.js
+++ src/consts.js
@@ -4,179 +4,122 @@ export const logoImageWidth = 250;
 export const logoImageHeight = 80;
 
 export const config = {
-        "name": "Nav Bar Template Clone",
-        "height": 59,
-        "logoSrc": "https://api.ifolio.cloud/medias/By/6I/By6IfDU0klbCi3blQAGMR0WgMF83dxq1DNuK6wfO.png",
-        "structure": [
-            {
-                "id": "c8eb4d50-2dc3-11ed-ae24-1b53f41c03b5",
-                "link": {
-                    "type": "link",
-                    "value": "https://ifoliocorp.com/"
-                },
-                "type": "page",
-                "title": "SERVICES",
-                "children": [],
-                "expanded": true
-            },
-            {
-                "id": "c8eb4d50-2dc3-11ed-ae24-1b53441c03b5",
-                "link": {
-                    "type": "link",
-                    "value": "https://ifoliocorp.com/"
-                },
-                "type": "page",
-                "title": "SERVICES 2",
-                "children": [],
-                "expanded": true
-            },
-            {
-                "id": "18eb4d50-2dc3-11ed-ae24-1b53441c03b5",
-                "link": {
-                    "type": "link",
-                    "value": "https://ifoliocorp.com/"
-                },
-                "type": "page",
-                "title": "SERVICES 3",
-                "children": [],
-                "expanded": true
-            },
-            {
-                "id": "bfe87a70-2dc3-11ed-ae24-1b53f41c03b5",
-                "link": null,
-                "type": "page",
-                "title": "ABOUT",
-                "children": [
-                    {
-                        "id": "fd95d0c0-2dc3-11ed-a7eb-c30aa007ce78",
-                        "link": {
-                            "type": "link",
-                            "value": "https://ifoliocorp.com/"
-                        },
-                        "type": "page",
-                        "title": "Success stories",
-                        "children": []
-                    },
-                    {
-                        "id": "efc59750-2dc3-11ed-a7eb-c30aa007ce78",
-                        "link": {
-                            "type": "link",
-                            "value": "https://ifoliocorp.com/"
-                        },
-                        "type": "page",
-                        "title": "Our company",
-                        "children": []
-                    },
-                    {
-                        "id": "c8d2c6d0-2dc4-11ed-a7eb-c30aa007ce78",
-                        "link": null,
-                        "type": "page",
-                        "title": "Our approach",
-                        "children": []
-                    }
-                ],
-                "expanded": true
-            },
-            {
-                "id": "c4f3ad50-2dc3-11ed-ae24-1b53f41c03b5",
-                "link": {
-                    "type": "link",
-                    "value": "https://ifoliocorp.com/"
-                },
-                "type": "page",
-                "title": "INDUSTRIES",
-                "children": [
-                    {
-                        "id": "ba430df0-2dc4-11ed-a7eb-c30aa007ce78",
-                        "link": null,
-                        "type": "page",
-                        "title": "Marketing",
-                        "children": []
-                    },
-                    {
-                        "id": "bed196c0-2dc4-11ed-a7eb-c30aa007ce78",
-                        "link": null,
-                        "type": "page",
-                        "title": "Sales",
-                        "children": []
-                    }
-                ],
-                "expanded": true
-            },
-            {
-                "id": "46a21260-2dc4-11ed-a7eb-c30aa007ce78",
-                "link": null,
-                "type": "button",
-                "title": "LOG-IN",
-                "children": []
-            }
-        ],
-        "mobileHeight": 65,
-        "logoAlignment": "left",
-        "mobileFontSize": 14,
-        "navBarBehavior": "frozen",
-        "navBarTextBold": true,
-        "navBarHoverColor": "#41c5c2",
-        "navBarTextItalic": false,
-        "navBarTextSpacing": 27,
-        "logoCropperOptions": {
-            "x": 0,
-            "y": -32.222,
-            "zoom": 1.0086455331412103,
-            "width": 349.774,
-            "height": 174.887,
-            "rotate": 0,
-            "scaleX": 1,
-            "scaleY": 1,
-            "imageBox": {
-                "x": 0,
-                "y": 0,
-                "width": 347,
-                "height": 104
-            },
-            "imgWidth": 347,
-            "imgHeight": 104,
-            "imagePosition": {
-                "x": 0,
-                "y": 18.43,
-                "width": 99.21,
-                "height": 59.47
-            }
+    "name": "iFOLIO Cloud Nav Bar",
+    "height": 51,
+    "logoSrc": "https://api.ifolio.cloud/medias/IA/BL/IABLlu75QaNFlnKKhHpuOlBnQqkouLacPCGHjqR2.png",
+    "structure": [
+        {
+            "id": "238dd280-5931-11ed-a28b-ef989d3b3c05",
+            "link": null,
+            "type": "page",
+            "title": "ABOUT",
+            "children": []
         },
-        "navBarClickedColor": "#41c5c2",
-        "navBarRegularColor": "#41c5c2",
-        "navBarTextFontSize": 14,
-        "mobileLogoAlignment": "right",
-        "mobileTextAlignment": "center",
-        "navBarTextAlignment": "center",
-        "navBarTextUnderline": false,
-        "navBarTextFontFamily": "Avenir-heavy",
-        "navBarTextHoverColor": "#292e50",
-        "navBarTextClickedColor": "#292e50",
-        "navBarTextRegularColor": "#ffffff",
-        "dropdownSettingsTextBold": false,
-        "buttonSettingsBorderWidth": 2,
-        "buttonSettingsRegularFill": "transparent",
-        "buttonSettingsRoundCorners": 20,
-        "dropdownSettingsTextItalic": false,
-        "buttonSettingsRegularBorder": "#fff",
-        "buttonSettingsTextColorHover": "#fff",
-        "dropdownSettingsInactiveFill": "#2cbab7",
-        "dropdownSettingsTextFontSize": 14,
-        "buttonSettingsVerticalPadding": 8,
-        "dropdownSettingsTextUnderline": false,
-        "buttonSettingsHoverClickedFill": "#292e50",
-        "buttonSettingsTextColorClicked": "#fff",
-        "buttonSettingsTextColorRegular": "#fff",
-        "dropdownSettingsInactiveBorder": "#2cbab7",
-        "dropdownSettingsTextColorHover": "#41c5c2",
-        "dropdownSettingsTextFontFamily": "Roboto",
-        "buttonSettingsHorizontalPadding": 20,
-        "dropdownSettingsVerticalPadding": 8,
-        "buttonSettingsHoverClickedBorder": "#fff",
-        "dropdownSettingsHoverClickedFill": "#eeffff",
-        "dropdownSettingsTextColorClicked": "#41c5c2",
-        "dropdownSettingsTextColorRegular": "#fff",
-        "dropdownSettingsHorizontalPadding": 10,
-        "dropdownSettingsHoverClickedBorder": "#eeffff"
-    }
+        {
+            "id": "2ccff850-5931-11ed-a28b-ef989d3b3c05",
+            "link": null,
+            "type": "page",
+            "title": "ANALYTICS",
+            "children": []
+        },
+        {
+            "id": "3248ba10-5931-11ed-a28b-ef989d3b3c05",
+            "link": null,
+            "type": "page",
+            "title": "CAMPAIGNS",
+            "children": []
+        },
+        {
+            "id": "2a5a8b80-5931-11ed-a28b-ef989d3b3c05",
+            "link": null,
+            "type": "page",
+            "title": "PRICING",
+            "children": [
+                {
+                    "id": "37c684e0-5931-11ed-a28b-ef989d3b3c05",
+                    "link": null,
+                    "type": "page",
+                    "title": "CONTACT",
+                    "children": []
+                }
+            ],
+            "expanded": true
+        },
+        {
+            "id": "1d764340-5932-11ed-a28b-ef989d3b3c05",
+            "link": null,
+            "type": "button",
+            "title": "LOG-IN",
+            "children": []
+        }
+    ],
+    "mobileHeight": 67,
+    "logoAlignment": "left",
+    "mobileFontSize": 14,
+    "navBarBehavior": "frozen",
+    "navBarTextBold": true,
+    "navBarHoverColor": "#ffffff",
+    "navBarTextItalic": false,
+    "navBarTextSpacing": 10,
+    "logoCropperOptions": {
+        "x": -3.026,
+        "y": -11.141,
+        "zoom": 0.5691982436168481,
+        "width": 560.456,
+        "height": 91.483,
+        "rotate": 0,
+        "scaleX": 1,
+        "scaleY": 1,
+        "imageBox": {
+            "x": 0,
+            "y": 0,
+            "width": 559,
+            "height": 68
+        },
+        "imgWidth": 559,
+        "imgHeight": 68,
+        "imagePosition": {
+            "x": 0.54,
+            "y": 12.18,
+            "width": 99.75,
+            "height": 74.34
+        }
+    },
+    "navBarClickedColor": "#41c5c2",
+    "navBarRegularColor": "#000",
+    "navBarTextFontSize": 14,
+    "mobileLogoAlignment": "right",
+    "mobileTextAlignment": "left",
+    "navBarTextAlignment": "right",
+    "navBarTextUnderline": false,
+    "navBarTextFontFamily": "Avenir-heavy",
+    "navBarTextHoverColor": "#25bec8",
+    "navBarTextClickedColor": "#ffffff",
+    "navBarTextRegularColor": "#ffffff",
+    "dropdownSettingsTextBold": false,
+    "buttonSettingsBorderWidth": 2,
+    "buttonSettingsRegularFill": "transparent",
+    "buttonSettingsRoundCorners": 20,
+    "dropdownSettingsTextItalic": false,
+    "navBarRegularOnScrollColor": "#1f2741",
+    "buttonSettingsRegularBorder": "#fff",
+    "buttonSettingsTextColorHover": "#1f2741",
+    "dropdownSettingsInactiveFill": "#2cbab7",
+    "dropdownSettingsTextFontSize": 14,
+    "buttonSettingsVerticalPadding": 8,
+    "dropdownSettingsTextUnderline": false,
+    "buttonSettingsHoverClickedFill": "#ffffff",
+    "buttonSettingsTextColorClicked": "#1f2741",
+    "buttonSettingsTextColorRegular": "#fff",
+    "dropdownSettingsInactiveBorder": "#2cbab7",
+    "dropdownSettingsTextColorHover": "#41c5c2",
+    "dropdownSettingsTextFontFamily": "Arial",
+    "buttonSettingsHorizontalPadding": 20,
+    "dropdownSettingsVerticalPadding": 8,
+    "buttonSettingsHoverClickedBorder": "#fff",
+    "dropdownSettingsHoverClickedFill": "#eeffff",
+    "dropdownSettingsTextColorClicked": "#41c5c2",
+    "dropdownSettingsTextColorRegular": "#fff",
+    "dropdownSettingsHorizontalPadding": 10,
+    "dropdownSettingsHoverClickedBorder": "#eeffff"
+}
